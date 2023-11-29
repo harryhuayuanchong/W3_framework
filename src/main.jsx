8da9b98d7357c615b927fa69bb4450a5cc2b2af8
harryhuayuanchong <harryhuayuanchong@gmail.com>
2023-11-29 12:34:51 +0800

    chore: Wagmi config

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,40 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { store } from './store';
-import { Provider } from "react-redux"; 
+import { Provider } from "react-redux";
+import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import { getDefaultProvider } from 'ethers';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { InjectedConnector } from 'wagmi/connectors/injected'
+
+// QuickNode API：Provider
+const quickNodeApiKey = 'YOUR_QUICKNODE_API_KEY';
+
+// Configure Chain & Provider
+const { chains, provider } = configureChains(
+  [mainnet, sepolia],
+  [
+    alchemyProvider({ apiKey: quickNodeApiKey }),
+    publicProvider()
+  ]
+)
+
+// Create Wagmi Config
+const config = createConfig({
+  autoConnect: true,
+  provider,
+  connectors: [new InjectedConnector({ chains })],
+  WebSocketProvider: getDefaultProvider(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <WagmiConfig config={config} >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </WagmiConfig>
   </React.StrictMode>,
 )
