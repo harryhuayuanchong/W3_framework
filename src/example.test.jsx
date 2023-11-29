4f0302a41971c74c3592341e797a04f18798e24e
harryhuayuanchong <harryhuayuanchong@gmail.com>
2023-11-29 12:45:20 +0800

    chore: Jest config

---
+++ src/example.test.jsx
@@ -0,0 +1,10 @@
+// ExampleComponent.test.jsx
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import ExampleComponent from './ExampleComponent';
+
+// test('renders learn react link', () => {
+//   render(<ExampleComponent />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
